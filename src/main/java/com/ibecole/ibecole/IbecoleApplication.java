743a2447c310c800641b315ced26792c17644c73
BOUZIDI Mehdi <mehdi.bouzidi.92@gmail.com>
2018-12-01 20:59:55 +0100

    Packages + Database Connection

--- src/main/java/com/ibecole/ibecole/IbecoleApplication.java
+++ src/main/java/com/ibecole/ibecole/IbecoleApplication.java
@@ -1,6 +1,7 @@
 package com.ibecole.ibecole;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
