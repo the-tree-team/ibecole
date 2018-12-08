f75c1c17d0a0361ff052a8783d76061e0f85535f
BOUZIDI Mehdi <mehdi.bouzidi.92@gmail.com>
2018-12-08 21:04:45 +0100

    Sanction

--- src/main/java/com/ibecole/ibecole/entity/business/Sanction.java
+++ src/main/java/com/ibecole/ibecole/entity/business/Sanction.java
@@ -1,13 +1,16 @@
 package com.ibecole.ibecole.entity.business;
 
 import com.ibecole.ibecole.commun.StaticUtil;
-import lombok.*;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
-import javax.xml.bind.annotation.XmlRootElement;
 import java.io.Serializable;
 import java.time.LocalDate;
+import javax.xml.bind.annotation.XmlRootElement;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
 @Table(schema = StaticUtil.SCH_BUSINESS, name = "sanction")
@@ -16,8 +19,6 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @ToString(of = "id", doNotUseGetters = true)
 @EqualsAndHashCode(of = "id", doNotUseGetters = true)
-@Getter
-@Setter
 public class Sanction implements Serializable {
 
     @Id
@@ -33,18 +34,9 @@ public class Sanction implements Serializable {
 
     @Column(name = "date")
     private LocalDate date;
-/*
-    @NotNull
-    @Column(name = "type_sanction", length = 50, nullable = false)
-    @Enumerated(EnumType.STRING)
-    private TypeSanction typeSanction;*/
 
-    @JoinTable(
-            schema = StaticUtil.SCH_BUSINESS,
-            name = "join_type_sanction",
-            joinColumns = @JoinColumn(name = "type_fk"),
-            inverseJoinColumns = @JoinColumn(name = "sanction_fk")
-    )
+
+
     @ManyToOne
     private TypeSanction type;
 
