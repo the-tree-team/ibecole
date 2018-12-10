882b64e68ec7b79a4b8240126e418fd0bbc4f0a9
idireneyoucef <youcef_idi@hotmail.com>
2018-12-10 23:43:55 +0100

    professeur Converter

--- src/main/java/com/ibecole/ibecole/convertor/PersonneRequestToProfesseurConverter.java
+++ src/main/java/com/ibecole/ibecole/convertor/PersonneRequestToProfesseurConverter.java
@@ -1,4 +1,30 @@
 package com.ibecole.ibecole.convertor;
 
-public class PersonneRequestToProfesseurConverter {
+import com.ibecole.ibecole.commun.enumerate.Sexe;
+import com.ibecole.ibecole.entity.business.Parent;
+import com.ibecole.ibecole.entity.business.Professeur;
+import com.ibecole.ibecole.model.request.PersonneRequest;
+
+import java.util.function.Function;
+
+public class PersonneRequestToProfesseurConverter implements Function<PersonneRequest, Professeur> {
+    @Override
+    public Professeur apply(PersonneRequest personneRequest) {
+        Professeur professeur = new Professeur();
+        professeur.setNom(personneRequest.getNom());
+        professeur.setPrenom(personneRequest.getPrenom());
+        professeur.setSexe(Sexe.getSexeByLibelle(personneRequest.getSexe()));
+        professeur.setDateNaissance(personneRequest.getDateNaissance());
+        professeur.setLieuNaissance(personneRequest.getLieuNaissance());
+        professeur.setAdresse(personneRequest.getAdresse());
+        professeur.setTelephone(personneRequest.getTelephone());
+        professeur.setEmail(personneRequest.getEmail());
+        professeur.setPhoto(personneRequest.getPhoto());
+          /* Attributs Professeur */
+        professeur.setDateRecrutement(personneRequest.getDateRecrutement());
+        professeur.setEnseigner(personneRequest.getEnseigner());
+         return professeur;
+
+    }
+
 }
